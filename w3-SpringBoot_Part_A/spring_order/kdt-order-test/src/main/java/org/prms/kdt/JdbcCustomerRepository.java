44a5811b5043028e215828dd92611d8e562c5e59
sds1vrk <sdsvrk@gmail.com>
2021-08-26 17:11:49 +0900

    Code Block 처리를 통한 Auto close

--- w3-SpringBoot_Part_A/spring_order/kdt-order-test/src/main/java/org/prms/kdt/JdbcCustomerRepository.java
+++ w3-SpringBoot_Part_A/spring_order/kdt-order-test/src/main/java/org/prms/kdt/JdbcCustomerRepository.java
@@ -11,35 +11,26 @@ public class JdbcCustomerRepository {
     private static final Logger logger= LoggerFactory.getLogger(JdbcCustomerRepository.class);
     public static void main(String[] args) {
         // 원래 ID 패스워드는 들어가면 안됨 -> 설정 값으로 다 빼줘야 됨
-        Connection connection=null;
-        Statement statement=null;
-        ResultSet resultSet=null;
-        try {
-            connection=DriverManager.getConnection("jdbc:mysql://localhost/order_mgmt","root","root1234!");
-            statement=connection.createStatement();
-            resultSet=statement.executeQuery("select * from customers");
 
+        try (
+            var connection=DriverManager.getConnection("jdbc:mysql://localhost/order_mgmt","root","root1234!");
+            var statement=connection.createStatement();
+            var resultSet=statement.executeQuery("select * from customers");
+        )
+            // Auto Close -> 블록이 끝났을때 알아서 클로스 
+        {
             while (resultSet.next()) {
                 var name=resultSet.getString("name");
                 var customerID= UUID.nameUUIDFromBytes(resultSet.getBytes("customer_id")); //UUID는 bytes로 가져온다.
                 logger.info("customers name->{}, id->{}",name,customerID);
 
             }
-
-        } catch (SQLException sqlException) {
+        }
+        catch (SQLException sqlException) {
             sqlException.printStackTrace();
             logger.error("GOT error while createStatement connection",sqlException);
         }
-        finally {
-            // 반드시 닫아줘야 함.
-            try {
-                if (connection!=null) connection.close();
-                if (statement!=null) statement.close();
-                if (resultSet!=null) resultSet.close();
-            }catch (SQLException sqlException) {
-                logger.error("GOT error while closing connection",sqlException);
-            }
-        }
+
 
     }
 }
